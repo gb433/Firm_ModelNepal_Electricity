d14a8c93f59db74b53cb3a9c83dc7f437cea0ef7
Geeta Bhatta <39402675+gb433@users.noreply.github.com>
2025-05-27 22:06:40 +0545

    Commit

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # python3 Optimisation.py -e 6 -s existing -y no_import -p 10 -i 10
-python3.11 Optimisation.py -e 2 -s existing -n Super -y no_import -f HVAC -p 10 -i 1
+python3.11 Optimisation.py -e 2 -s existing -n Super -y no_import -p 10 -i 1
 # python3 Optimisation.py -e 3 -s existing -n APG_MY_Isolated -y no_import -f HVAC -i 3 -p 5
 
 
